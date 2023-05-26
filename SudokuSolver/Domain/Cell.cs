b568edaddde236ccf918f690739d56d9684bef81
Konstantin Lapshin <kostiantyn.lapshyn@dataart.com>
2023-05-26 17:01:24 +0300

    Added Cell to the domain models

---
+++ SudokuSolver/Domain/Cell.cs
@@ -0,0 +1,29 @@
+﻿namespace SudokuSolver.domain;
+
+public class Cell
+{
+    public int Value { get; set; }
+    public HashSet<int> PossibleValues { get; set; }
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public int Block { get; set; }
+
+    public Cell(int row, int column)
+    {
+        Value = 0;
+        PossibleValues = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Row = row;
+        Column = column;
+
+    }
+
+    public void SetValue(int value)
+    {
+        if (Value < 1 || Value > 9) 
+        {
+            throw new ArgumentException($"The value: {value} is incorrect. Must be between 1 and 9.");
+        }
+    }
+
+}
+
--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+for (int row = 0; row < 9; row++)
+    for (int col = 0; col < 9; col++)
+    {
+        int block = 3 * (row / 3) + (col / 3);
+        Console.WriteLine($"({row},{col}) => {block}");
+    }
