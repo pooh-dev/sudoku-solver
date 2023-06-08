115c79e3407b7439aa51d27084a2176109a80c8e
Konstantin Lapshin <kostiantyn.lapshyn@dataart.com>
2023-06-08 13:41:04 +0300

    Updated 'Cell' model

--- SudokuSolver/Domain/Cell.cs
+++ SudokuSolver/Domain/Cell.cs
@@ -3,15 +3,15 @@
 public class Cell
 {
     public int Value { get; set; }
-    public HashSet<int> PossibleValues { get; set; }
-    public int Row { get; set; }
-    public int Column { get; set; }
-    public int Block { get; set; }
+    public int Row { get; }
+    public int Column { get; }
+    public int Block { get; }
+    private readonly HashSet<int> possibleValues;
 
     public Cell(int row, int col)
     {
         Value = 0;
-        PossibleValues = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        possibleValues = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         Row = row;
         Column = col;
         // only the integer part of the number is needed
@@ -21,11 +21,10 @@ public class Cell
     public void SetValue(int value)
     {
         Value = value;
-        PossibleValues.Clear();
+        possibleValues.Clear();
     }
 
-    public void RemoveFromPossibleValues(int value)
-    {
-        PossibleValues.Remove(value);
-    }
+    public void RemoveFromPossibleValues(int value) => possibleValues.Remove(value);
+
+    public bool IsOpened() => Value > 0;
 }
