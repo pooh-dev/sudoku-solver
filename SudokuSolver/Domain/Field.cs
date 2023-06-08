4e69199c81657579f0d3f5296fdcf4341e69750b
Konstantin Lapshin <kostiantyn.lapshyn@dataart.com>
2023-06-08 16:07:58 +0300

    Added the 'Init' method to the 'Field' model

--- SudokuSolver/Domain/Field.cs
+++ SudokuSolver/Domain/Field.cs
@@ -18,6 +18,31 @@ public class Field
         }
     }
 
+    public void Init(List<string> rows)
+    {
+        for (int row = 0; row < 9; row++) 
+        {
+            var rowValues = rows[row]
+                .Split(',')
+                .Select(int.Parse)
+                .ToList();
+            
+            for (int col = 0; col < 9; col++)
+            {
+                var cell = GetCell(row, col) ?? throw new ArgumentNullException($"There is no Cell({row},{col}");
+                cell.SetValue(rowValues[col]);
+                RemovePossibleValuesFromRelatedCells(cell);
+            }
+        }
+    }
+
+    public Cell? GetCell(int row, int col)
+    {
+        return _cells
+            .Where(c => c.Row == row && c.Column == col)
+            .FirstOrDefault();
+    }
+
     public override string? ToString()
     {
         StringBuilder sb = new();
@@ -33,5 +58,10 @@ public class Field
             sb.Append("\n\n");
         }
         return sb.ToString();
+    }
+
+    private void RemovePossibleValuesFromRelatedCells(Cell cell)
+    {
+        
     }
 }
